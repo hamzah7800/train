1d248e858caf6e67a672722ac7f8399a05c2f16d
hamzah7800 <hzeb7800pass@gmail.com>
2025-05-23 09:48:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.getElementById("leftBtn").addEventListener("touchstart", () => game.moveTrain("left"));
   document.getElementById("rightBtn").addEventListener("touchstart", () => game.moveTrain("right"));
+  document.getElementById("upBtn").addEventListener("touchstart", () => game.moveTrain("up"));
+  document.getElementById("downBtn").addEventListener("touchstart", () => game.moveTrain("down"));
 });
