a5c2100fd2bbe1316fe7ae83f07f82caea146a34
hamzah7800 <hzeb7800pass@gmail.com>
2025-05-23 09:31:36 +0100

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const game = new TrainGame();
-  game.start();
-
-  document.getElementById("upBtn").addEventListener("touchstart", () => game.moveTrain("up"));
-  document.getElementById("downBtn").addEventListener("touchstart", () => game.moveTrain("down"));
-});
